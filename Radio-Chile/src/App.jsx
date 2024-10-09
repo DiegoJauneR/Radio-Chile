efc5ce38380c11b1b5eac44544186eab9abed6ab
Tiannnnnnnnnnnnn <111377366+Tiannnnnnnnnnnnn@users.noreply.github.com>
2024-10-09 00:03:51 -0300

    Funcionalidad de botones

--- Radio-Chile/src/App.jsx
+++ Radio-Chile/src/App.jsx
@@ -4,6 +4,9 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
+  const [iterador, setIterador] = useState(0)
+  const [busqueda, setBusqueda] = useState()
+  const [radiosFiltradas, setRadiosFiltradas] = useState([])
 
   useEffect(() => {
     const url = "https://api.boostr.cl/radios.json";
@@ -15,12 +18,65 @@ function App() {
       .catch((err) => console.error("error:" + err));
   }, []);
 
+  const siguienteRadio = () => {
+    if(iterador === data.length-1) {
+      setIterador(0)
+      return
+    }
+    console.log(data.length)
+    setIterador(iterador+1)
+  }
+
+  const anteriorRadio = () => {
+    if(iterador === 0) {
+      setIterador(data.length-1)
+      return
+    }
+    setIterador(iterador-1)
+  }
+
+  const buscarRadio = () =>{
+    const iterador2 = (elemento) => elemento.name === busqueda
+    const iteradorBuscado = data.findIndex(iterador2)
+    if(iteradorBuscado===-1) return
+    setIterador(iteradorBuscado) 
+  }
+
+  const filtrarBusqueda = (event) => {
+    const value = event.target.value
+    setBusqueda(value)
+
+    
+    const resultadosFiltrados = data.filter((radio) =>
+      radio.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setRadiosFiltradas(resultadosFiltrados)
+  };
+
+  const rellenarBusqueda = (nombreRadio) =>{
+    setBusqueda(nombreRadio)
+    setRadiosFiltradas([])
+  }
+
   return (
     <main>
       <div>
         <p>Radios Chilenas</p>
       </div>
-      <Radio data={data}></Radio>
+      <input type="search" placeholder="Buscar Radio" value={busqueda} onChange={filtrarBusqueda}></input>
+      <button onClick={buscarRadio}>Buscar</button>
+      {busqueda && (
+        <ul>
+          {radiosFiltradas.map((radio, index) => (
+            <li key={index}>
+              <button onClick={()=>rellenarBusqueda(radio.name)}>{radio.name}</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <Radio data={data} iterador={iterador}></Radio>
+      <button onClick={anteriorRadio}>atras</button>
+      <button onClick={siguienteRadio}>siguiente</button>
     </main>
   );
 }
--- Radio-Chile/src/Radio.jsx
+++ Radio-Chile/src/Radio.jsx
@@ -1,23 +1,41 @@
-const Radio = ({ data }) => {
+import { useEffect } from 'react';
+
+const Radio = ({ data, iterador}) => {
+  const elemento = data[iterador]
+
   if (!data || data.length === 0) {
     return <p>No hay datos disponibles</p>;
   }
 
-  const elemento = data[0];
+  useEffect(() => {
+    const audioElement = document.getElementById("audioElement");
+    
+    if (audioElement) {
+      const playAudio = async () => {
+        try {
+          await audioElement.play()
+          console.log("Reproducción automática iniciada.");
+        } catch (error) {
+          console.error("La reproducción automática fue bloqueada:", error)
+        }
+      };
+
+      playAudio()
+    }
+  }, [elemento.stream])
 
   return (
     <section key={elemento.name}>
       <div>
         <div>
-          <p>{elemento.name}</p>
-          <p>{elemento.url}</p>
-          <audio controls>
-            <source src={elemento.stream} type="audio/aac" />
+          <img src={elemento.image[200]} alt="Logo de Radio" />
+        </div>
+        <a href={elemento.url}>{elemento.name}</a>
+        <div>
+          <audio controls id="audioElement" volume="0.7">
+            <source src={elemento.stream} type="audio/aac"/>
             Tu navegador no soporta el elemento de audio.
           </audio>
-        </div>
-        <div>
-          <img src={elemento.image[200]} alt="Logo de Radio" />
         </div>
       </div>
     </section>
